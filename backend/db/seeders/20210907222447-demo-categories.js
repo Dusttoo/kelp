13e83ed618cc3f7e996400b66e85783fc57f1651
Dusty Mumphrey <85353431+Dusttoo@users.noreply.github.com>
2021-10-11 07:09:48 -0500

    fixing database

---
+++ backend/db/seeders/20210907222447-demo-categories.js
@@ -0,0 +1,37 @@
+//delete
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+      return queryInterface.bulkInsert('Categories', [{
+      category: 'Fast Food',
+      numBusinesses: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      category: 'Bar',
+      numBusinesses: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      category: 'Education',
+      numBusinesses: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      category: 'Gift Shop',
+      numBusinesses: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+      return queryInterface.bulkDelete('Categories', null, {});
+
+  }
+};
+
